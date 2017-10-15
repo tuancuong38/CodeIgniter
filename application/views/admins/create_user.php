9425346f1fd9c1adb3c3e50915c3656d71c512d1
Ryan Truong <[tuancuong3886@gmail.com]>
2017-10-15 15:40:34 +0700

    update view create user

--- application/views/admins/create_user.php
+++ application/views/admins/create_user.php
@@ -68,9 +68,9 @@
             }
         </style>
         <script>
-            // function cancelForm() {
-            //     document.getElementById("createFrom").reset();
-            // }
+            function cancelForm() {
+                document.getElementById("createFrom").reset();
+            }
             
             // $(document).ready(function (){
             //     $('#register').click(function () {
@@ -109,36 +109,36 @@
                 
                 <form class="form" id="createFrom" method="POST" >
                     <div class="item">
-                    <?php echo validation_errors(); ?>
-                
                         <label>Họ:</label>
                         <input type="text" name="lastName"  value="<?php echo set_value('lastName') ?>"></br>
-                        <?php echo 'bbb'.form_error('lastName'); ?>
+                        <?php echo ''.form_error('lastName'); ?>
                        
                     </div>
                     <div class="item">
 
                         <label>Tên :</label>
                         <input type="text" name="firstName" value="<?php echo set_value("firstName")?>"></br>
-                        <p class="error" id="firstName" ></p> 
+                        <?php echo ''.form_error('firstName'); ?>
                     </div>
                     <div class="item">
 
                         <label>Ngày sinh:</label>
                         <input type="date" name="birth" value="<?php echo set_value("birth")?>"></br>
-                        <p class="error" id="birth"></p> 
+                        <?php echo ''.form_error('birth'); ?>
                     </div>
                     <div class="item">
 
                         <label>Email :</label>
                         <input type="email" name="email" value="<?php echo set_value("email")?>"></br>
-                        <p class="error" id="email"></p> 
+                        <p class="error" id="email"></p>
+                        <?php echo ''.form_error('email'); ?>
                     </div>
                     <div class="item">
 
                         <label>Mật khẩu :</label>
                         <input type="password" name="pass" ></br>
-                        <p class="error" id="pass"></p> 
+                        <p class="error" id="pass"></p>
+                        <?php echo ''.form_error('pass'); ?>
                     </div>
                     <div class="item">
 
@@ -153,13 +153,14 @@
 
                         <label>Phone :</label>
                         <input type="tel" name="phone" value="<?php echo set_value("phone")?>"></br>
-                        <p class="error" id="phone"></p> 
+                        <?php echo ''.form_error('phone'); ?>
                     </div>
                     <div class="item">
 
                         <label>Địa chỉ :</label>
                         <input type="text" name="address" value="<?php echo set_value("address")?>"></br>
-                        <p class="error" id="address"></p> 
+                        <p class="error" id="address"></p>
+                        <?php echo ''.form_error('address'); ?>
                     </div>
 
                     <div class="button">
